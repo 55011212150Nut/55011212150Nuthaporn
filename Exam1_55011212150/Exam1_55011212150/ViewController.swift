147abd0f22686022ad3f23167bf42729feab0900
55011212182Som <student@tests-iMac-7.local>
2014-12-17 12:28:10 +0700

    final

---
+++ Exam1_55011212150/Exam1_55011212150/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exam1_55011212150
+//
+//  Created by student on 12/17/2557 BE.
+//  Copyright (c) 2557 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Stocks/Stocks/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by student on 12/17/2557 BE.
+//  Copyright (c) 2557 student. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
